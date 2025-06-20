a450c75a8bb0972dfe6f1ff5818f7cb6a5913091
SaintenoyTom <tom.saintenoy@ecoles-epsi.net>
2025-06-20 09:37:25 +0200

    Add parameter

--- src/main/java/cicd/learn/tp1/App.java
+++ src/main/java/cicd/learn/tp1/App.java
@@ -6,8 +6,12 @@ package cicd.learn.tp1;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main(String[] args)
     {
-        System.out.println( "CI/CD rocks! test" );
+        if (args.length > 0) {
+            System.out.println("CI/CD rocks! " + args[0]);
+        } else {
+            System.out.println("CI/CD rocks!");
+        }
     }
 }
