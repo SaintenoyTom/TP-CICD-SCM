e2e59713898665094b2a5e29df12b2d28c330037
SaintenoyTom <tom.saintenoy@ecoles-epsi.net>
2025-06-20 09:43:18 +0200

    some works on feature #003

--- src/main/java/cicd/learn/tp1/Wordcounter.java
+++ src/main/java/cicd/learn/tp1/Wordcounter.java
@@ -1,5 +1,8 @@
 package cicd.learn.tp1;
 
 public class Wordcounter {
-    
+    public static void main( String[] args )
+    {
+        System.out.println( "Wordcounter (flemme de le coder)" );
+    }
 }
